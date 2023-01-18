d689be89689345b6fa53ac49e9735335191fc60d
Jeevan Padwal <105958058+jeevanpadwal@users.noreply.github.com>
2023-01-18 20:47:19 +0530

    Add files via upload

--- friend.cpp
+++ friend.cpp
@@ -1,44 +1,42 @@
 #include<iostream>
-
-
 using namespace std;
 
 class Demo
 {
- pubilc:
- int i;
-
- private:
-
- int j;
-
-  protected:
-
- int k;
-
- public:
-
-  Demo()
- {
-
-    i=10;
-    j=20;
-    k=30;
- } 
- friend void fun()
+	public:
+	int i;
+	private:
+	int j;
+	protected:
+	int k;
+	
+	public:
+	
+	Demo()
+	{
+		i=10;
+		j=20;
+		k=30;
+		
+	}
+	
+	friend void fun()
+	
 };
+     
+     void fun()
+   { 
+    Demo dobj1;
+	cout<<dobj1.i<<"\n";
+	cout<<dobj1.j<<"\n";
+	cout<<dobj1.k<<"\n";
+   }
+   
+   int main()
+   {
+	 Demo dobj;
 
-void fun()
-{
-     Demo obj;
-     cout<<obj.i;
-     cout<<obj.j<<"\n";
-     cout<<obj.k<<"\n";
-}   
 
-
-void main()
-{
-    Demo obj1;
-    obj1.fun() 
-}    
+       return 0;	 
+	   
+   }
